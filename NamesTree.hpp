0af6a893cd986b924ef5f3af9649e74c08bb5cc4
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-03-17 20:19:16 -0400

    Create NamesTree.hpp

---
+++ NamesTree.hpp
@@ -0,0 +1,17 @@
+#ifndef NAMES_TREE_HPP
+#define NAMES_TREE_HPP
+
+#include "BST.hpp"
+#include "NamesTreeNode.hpp"
+
+#include <iostream>
+
+class NamesTree {
+public:
+
+private:
+    BST<NamesTreeNode> names;
+};
+
+
+#endif
